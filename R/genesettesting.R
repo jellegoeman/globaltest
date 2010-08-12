75d6ee9c875fe7f414261ca705dedca661e57da8
J.J. Goeman <J.J.Goeman@lumc.nl>
2010-08-12 15:51:51 +0000

    - added the gtConcept function to the package
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/globaltest@48767 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genesettesting.R
+++ R/genesettesting.R
@@ -298,3 +298,120 @@ gtBroad <- function(response, exprs, ..., id, annotation, probe2entrez, collecti
   
   res
 }
+
+
+###################################################
+# Function for using Anni concept profiles
+###################################################
+gtConcept <- function(response, exprs, ..., annotation, probe2entrez,
+                  conceptmatrix, concept2name = "conceptID2name.txt",
+                  entrez2concept = "entrezGeneToConceptID.txt", threshold = 1e-4,
+                  share = TRUE, multtest = c("Holm", "BH", "BY"),
+                  sort = TRUE) {
+
+  # some input checking on response (unavoidable?)
+  call <- match.call()
+  if (is(exprs, "eSet")) {
+    require("Biobase") || stop("ExpressionSet input but Biobase package not available")
+    data <- pData(exprs)
+  } else {
+    data <- eval(call$data)
+  }
+  if (is.matrix(data))
+    data <- data.frame(data)
+  formula.response <- (length(call$response) > 1) && (call$response[[1]] == "~")
+  if (!formula.response) {
+    if (length(call$response) > 1 && deparse(call$response[1]) == "Surv()")
+      require("survival") || stop("Surv input but survival package not available")
+    name.response <- deparse(call$response)
+    response <- eval(call$response, data, parent.frame())
+  }
+
+  # get the right annotation package
+  if (missing(annotation)) {
+    if (is(exprs, "eSet")) {
+      annotation <- NULL
+      annotation <- annotation(exprs)
+    } else
+      stop("argument \"annotation\" is missing with no default.")
+  }
+  if (substr(annotation, nchar(annotation)-2, nchar(annotation)) == ".db")
+    annotation <- substr(annotation, 1, nchar(annotation)-3)
+  package <- paste(annotation, ".db", sep="")
+  require(package, character.only=TRUE) || stop("package ", package, " is not available")
+  require(annotate) || stop("package annotate is not available")
+
+  # read the big concept profiles matrix
+  nconcepts <- length(scan(conceptmatrix, what="", sep="\t", nlines=1, quiet=TRUE))
+  conceptmatrix <- scan(conceptmatrix, what="", sep="\t", quiet=TRUE)
+  conceptmatrix <- matrix(conceptmatrix, length(conceptmatrix)/nconcepts, nconcepts, byrow=TRUE)
+  rnames <- conceptmatrix[-1,1]
+  cnames <- conceptmatrix[1,-1]
+  conceptmatrix <- apply(conceptmatrix[-1,-1],2,as.numeric)
+  rownames(conceptmatrix) <- rnames
+  colnames(conceptmatrix) <- cnames
+
+  # use the threshold
+  conceptmatrix[conceptmatrix < threshold] <- 0
+
+  # prepare entrez2concept
+  entrez2concept <- matrix(scan(entrez2concept, what="", sep="\t", skip=1, quiet=TRUE), ncol=2, byrow=TRUE)
+  nms <- entrez2concept[,1]
+  entrez2concept <- as.list(entrez2concept[,2])
+  names(entrez2concept) <- nms
+
+  # prepare concept2probe mapping
+  if (is(exprs, "eSet"))
+    probes <- featureNames(exprs)
+  else
+    probes <- rownames(exprs)
+  if (missing(probe2entrez))
+    probe2entrez <- lookUp(probes, annotation, "ENTREZID")
+  else
+    if (is.environment(probe2entrez) || is(probe2entrez, "AnnDbBimap"))
+      probe2entrez <- as.list(probe2entrez)
+  probe2entrez <- lapply(probe2entrez, as.character)
+  probe2concept <- entrez2concept[unlist(probe2entrez)]
+  names(probe2concept) <- names(probe2entrez)
+  probe2concept <- probe2concept[sapply(probe2concept, length) > 0]
+
+  # find duplicity of concepts and correct weights
+  concept2probe <- turnListAround(probe2concept)
+  duplicity <- sapply(concept2probe[rnames], length)
+  names(duplicity) <- rnames
+  if (share)     # divides weight of concept equally over multiple probes
+    conceptmatrix <- conceptmatrix / matrix(duplicity, nrow = nrow(conceptmatrix), ncol=ncol(conceptmatrix))
+  conceptmatrix <- conceptmatrix[duplicity>0,]
+
+  # find weights
+  weights <- lapply(probe2concept, function(pb)
+    if(pb %in% rownames(conceptmatrix))
+      conceptmatrix[pb,]
+    else
+      NULL)
+  weights <- do.call(rbind, weights)
+  weights <- apply(weights, 2, function(cp) cp[cp>0])
+
+  # find alias
+  concept2name <- matrix(scan(concept2name, what="", sep="\t", skip=1, quote="", quiet=TRUE), ncol=2, byrow=TRUE)
+  nms <- concept2name[,1]
+  concept2name <- as.list(concept2name[,2])
+  names(concept2name) <- nms
+
+  # perform tests and do multiple testing
+  if (length(weights) > 1) {
+    multtest <- match.arg(multtest)
+    res <- p.adjust(gt(response, exprs, ..., weights = weights), method = multtest)
+  } else
+    res <- gt(response, exprs, ..., weights = weights)
+
+  # add names
+  alias(res) <- concept2name[names(res)]
+
+  if (sort)
+    res <- sort(res)
+
+  res
+}
+
+
